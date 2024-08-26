2fd2c9b8ea0124b56aa7d3eae6fbf15edbd8c6ca
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-08-26 22:48:27 +0700

    menambah fitur buku besar dan laba rugi

--- controllers/reports.js
+++ controllers/reports.js
@@ -71,6 +71,208 @@ exports.getReport = async (accountId) => {
     return final
 }
 
-exports.getBalanceSheet = async (accountCode) => {
-    return await db.query()
+exports.getLedger = async (accountId) => {
+    const query = await db.query(`SELECT 
+    je.date AS tanggal,
+    a.account_name AS nama,
+    a.account_code AS kode,
+    je.description AS deskripsi,
+    jd.debit AS debit,
+    jd.credit AS credit,
+    IFNULL(SUM(jd.debit - jd.credit) OVER (ORDER BY je.date, je.id), 0) AS saldo
+    FROM 
+        journal_details jd
+    INNER JOIN 
+        journal_entries je ON jd.journal_entry_id = je.id
+    INNER JOIN 
+        accounts a ON jd.account_id = a.id
+    INNER JOIN 
+        account_types aty ON a.account_type_id = aty.id
+    WHERE 
+        jd.account_id = ?
+    ORDER BY 
+    je.date, je.id;
+`, [accountId])
+    const result = {
+        "account_name": query[0].nama,
+        "account_code": query[0].kode,
+        "details": query.map(detail => ({
+            "date": detail.tanggal,
+            "description": detail.description,
+            "debit": detail.debit,
+            "credit": detail.credit,
+            "balance": detail.saldo,
+        }))
+    }
+    return result
+}
+exports.getProfitLoss = async (startDate, endDate) => {
+    const pendapatan = await db.query(`
+        SELECT 
+            a.account_code, 
+            a.account_name, 
+            SUM(IFNULL(jd.credit, 0) - IFNULL(jd.debit, 0)) AS saldo
+        FROM 
+            accounts a
+        JOIN 
+            account_types aty ON a.account_type_id = aty.id
+        LEFT JOIN 
+            journal_details jd ON a.id = jd.account_id
+        LEFT JOIN 
+            journal_entries je ON jd.journal_entry_id = je.id
+        WHERE 
+            aty.name = 'Pendapatan'
+            AND je.date BETWEEN ? AND ?
+        GROUP BY 
+            a.account_code, a.account_name;`,
+        [startDate, endDate]
+    );
+
+    const totalPendapatan = {
+        "details": pendapatan.map(detail => ({
+            "account_name": detail.account_name,
+            "account_code": detail.account_code,
+            "balance": detail.saldo,
+        })),
+        "total": pendapatan.reduce((acc, curr) => acc + curr.saldo, 0) // Menggunakan reduce untuk menghitung total saldo
+    };
+
+    const biayaAtasPendapatan = await db.query(`
+        SELECT 
+            a.account_code, 
+            a.account_name, 
+            SUM(IFNULL(jd.debit, 0) - IFNULL(jd.credit, 0)) AS saldo
+        FROM 
+            accounts a
+        JOIN 
+            account_types aty ON a.account_type_id = aty.id
+        LEFT JOIN 
+            journal_details jd ON a.id = jd.account_id
+        LEFT JOIN 
+            journal_entries je ON jd.journal_entry_id = je.id
+        WHERE 
+            aty.name = 'HPP'
+            AND je.date BETWEEN ? AND ?
+        GROUP BY 
+            a.account_code, a.account_name;`,
+        [startDate, endDate]
+    );
+
+    const totalBiayaAtasPendapatan = {
+        "details": biayaAtasPendapatan.map(detail => ({
+            "account_name": detail.account_name,
+            "account_code": detail.account_code,
+            "balance": detail.saldo,
+        })),
+        "total": biayaAtasPendapatan.reduce((acc, curr) => acc + curr.saldo, 0) // Menggunakan reduce untuk menghitung total saldo
+    };
+
+    const biaya = await db.query(`
+        SELECT 
+            a.account_code, 
+            a.account_name, 
+            SUM(IFNULL(jd.debit, 0) - IFNULL(jd.credit, 0)) AS saldo
+        FROM 
+            accounts a
+        JOIN 
+            account_types at ON a.account_type_id = at.id
+        LEFT JOIN 
+            journal_details jd ON a.id = jd.account_id
+        LEFT JOIN 
+            journal_entries je ON jd.journal_entry_id = je.id
+        WHERE 
+            at.name = 'Beban Biaya'
+            AND je.date BETWEEN ? AND ?
+        GROUP BY 
+            a.account_code, a.account_name;`,
+        [startDate, endDate]
+    );
+
+    const totalBiaya = {
+        "details": biaya.map(detail => ({
+            "account_name": detail.account_name,
+            "account_code": detail.account_code,
+            "balance": detail.saldo,
+        })),
+        "total": biaya.reduce((acc, curr) => acc + curr.saldo, 0) // Menggunakan reduce untuk menghitung total saldo
+    };
+
+    const pendapatanLainnya = await db.query(`
+        SELECT 
+            a.account_code, 
+            a.account_name, 
+            SUM(IFNULL(jd.credit, 0) - IFNULL(jd.debit, 0)) AS saldo
+        FROM 
+            accounts a
+        JOIN 
+            account_types at ON a.account_type_id = at.id
+        LEFT JOIN 
+            journal_details jd ON a.id = jd.account_id
+        LEFT JOIN 
+            journal_entries je ON jd.journal_entry_id = je.id
+        WHERE 
+            at.name = 'Pendapatan Lainnya'
+            AND je.date BETWEEN ? AND ?
+        GROUP BY 
+            a.account_code, a.account_name;`,
+        [startDate, endDate]
+    );
+
+    const totalPendapatanLainnya = {
+        "details": pendapatanLainnya.map(detail => ({
+            "account_name": detail.account_name,
+            "account_code": detail.account_code,
+            "balance": detail.saldo,
+        })),
+        "total": pendapatanLainnya.reduce((acc, curr) => acc + curr.saldo, 0) // Menggunakan reduce untuk menghitung total saldo
+    };
+
+    const bebanBiayaLainnya = await db.query(`
+        SELECT 
+            a.account_code, 
+            a.account_name, 
+            SUM(IFNULL(jd.debit, 0) - IFNULL(jd.credit, 0)) AS saldo
+        FROM 
+            accounts a
+        JOIN 
+            account_types at ON a.account_type_id = at.id
+        LEFT JOIN 
+            journal_details jd ON a.id = jd.account_id
+        LEFT JOIN 
+            journal_entries je ON jd.journal_entry_id = je.id
+        WHERE 
+            at.name = 'Beban Biaya Lainnya'
+            AND je.date BETWEEN ? AND ?
+        GROUP BY 
+            a.account_code, a.account_name;`,
+        [startDate, endDate]
+    );
+
+    const totalBebanBiayaLainnya = {
+        "details": bebanBiayaLainnya.map(detail => ({
+            "account_name": detail.account_name,
+            "account_code": detail.account_code,
+            "balance": detail.saldo,
+        })),
+        "total": bebanBiayaLainnya.reduce((acc, curr) => acc + curr.saldo, 0) // Menggunakan reduce untuk menghitung total saldo
+    };
+
+    const labaKotor = totalPendapatan.total - totalBiayaAtasPendapatan.total;
+    const labaOperasional = labaKotor - totalBiaya.total;
+    const totalPendapatanDanBiayaLain = totalPendapatanLainnya.total - totalBebanBiayaLainnya.total;
+    const labaBersih = labaOperasional + totalPendapatanDanBiayaLain;
+
+    const result = {
+        "report_type": "Laba Rugi",
+        "pendapatan": totalPendapatan,
+        "biaya_atas_pendapatan": totalBiayaAtasPendapatan,
+        "biaya": totalBiaya,
+        "laba_kotor": labaKotor,
+        "laba_operasional": labaOperasional,
+        "pendapatan_lainnya": totalPendapatanLainnya,
+        "beban_biaya_lainnya": totalBebanBiayaLainnya,
+        "pendapatan_dan_biaya_lainnya": totalPendapatanDanBiayaLain,
+        "laba_bersih": labaBersih
+    };
+    return result;
 }
--- routers/reports.js
+++ routers/reports.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const reports = express.Router()
-const { getFinalBalance, getReport } = require('../controllers/reports')
+const { getFinalBalance, getReport, getLedger, getProfitLoss } = require('../controllers/reports')
 
 reports.get('/:accountId', async (req, res) => {
     const { accountId } = req.params
     res.send(await getReport(accountId))
 })
-reports.get('/final-balance/:accountId', async (req, res) => {
+reports.get('/saldo-akhir/:accountId', async (req, res) => {
     const { accountId } = req.params
     res.send(await getFinalBalance(accountId))
+})
+reports.get('/buku-besar/:accountId', async (req, res) => {
+    const { accountId } = req.params
+    res.send(await getLedger(accountId))
+})
+reports.get('/laba-rugi/:startDate/:endDate', async (req, res) => {
+    const { startDate, endDate } = req.params
+    res.send(await getProfitLoss(startDate, endDate))
 })
 
 module.exports = reports
